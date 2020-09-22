8e433462d53ab904b36e8649b8ad7f9b5038fb6f
Scott Young <syoung@instructure.com>
2020-09-22 13:21:50 -0600

    add table for registrations

--- client/src/components/RegistrationIndex.js
+++ client/src/components/RegistrationIndex.js
@@ -1,49 +1,82 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import { ExternalLink } from 'react-external-link';
+import React, { Component } from "react";
+import { 
+    Table, 
+    Container,
+    Checkbox 
+} from "semantic-ui-react";
+import { ExternalLink } from "react-external-link";
 
-
-import axios from 'axios';
+import axios from "axios";
 
 class RegistrationIndex extends Component {
+  state = { registrations: [] };
 
-    state = { registrations: [] }
+  componentDidMount() {
+    axios
+      .get("/api/registrations")
+      .then((res) => {
+        this.setState({ registrations: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    componentDidMount() {
-        axios.get('/api/registrations')
-        .then(res => {
-            this.setState({ registrations: res.data })
-        }).catch( err =>{
-            console.log(err)
-        })
-    }
+  markAttended = (domain, session_id, registration_id) => {
+    const token = process.env.REACT_APP_BRIDGE_API_KEY
 
-    displayRegistrations = () => {
-        const { registrations } = this.state
-        const domain = 'syautocsv2'
-        return registrations.map(reg => (
-            <ExternalLink 
-            href={`https://${domain}.bridgeapp.com/author/training/${reg.live_course_id}/sessions/${reg.live_course_session_id}?search=${reg.uid}`}>
-                <Container key={reg.id}>
-                    <ul>
-                        <li>UID: {reg.uid}</li>
-                        <li>Live_course_id: {reg.live_course_id}</li>
-                        <li>Session_id: {reg.live_course_session_id}</li>
-                        <li>Registration_id: {reg.bridge_registration_id}</li>
-                    </ul>
-                </Container>
-            </ExternalLink>
-        ))
-    }
+    let config = {
+        headers: {
+          Authorization: token,
+        }
+      }
+      axios.patch(`https://${domain}.bridgeapp.com/api/author/live_course_sessions/${session_id}/registrations/${registration_id}`,{}, config)
+  }
 
-	render() {
-		return(
-            <div>
-                {this.displayRegistrations()}
-            </div>
-        )
-	}
+  displayRegistrations = () => {
+    const { registrations } = this.state;
+    const domain = "syautocsv2";
 
+    return (
+      <Container fluid>
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Unique ID</Table.HeaderCell>
+              <Table.HeaderCell>Live Course ID</Table.HeaderCell>
+              <Table.HeaderCell>Session ID</Table.HeaderCell>
+              <Table.HeaderCell>Registration ID</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {registrations.map((reg) => (
+              <Table.Row>
+                <Table.Cell collapsing>
+                  <Checkbox slider onClick={() => this.markAttended(domain, reg.live_course_session_id, reg.bridge_registration_id)}/>
+                </Table.Cell>
+
+                <Table.Cell>
+                  <ExternalLink
+                    key={reg.id}
+                    href={`https://${domain}.bridgeapp.com/author/training/${reg.live_course_id}/sessions/${reg.live_course_session_id}?search=${reg.uid}`}
+                  >
+                    {reg.uid}
+                  </ExternalLink>
+                </Table.Cell>
+                <Table.Cell>{reg.live_course_id}</Table.Cell>
+                <Table.Cell>{reg.live_course_session_id}</Table.Cell>
+                <Table.Cell>{reg.bridge_registration_id}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </Container>
+    );
+  };
+
+  render() {
+    return <div>{this.displayRegistrations()}</div>;
+  }
 }
 
 export default RegistrationIndex;
