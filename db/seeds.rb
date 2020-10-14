12921a287f038eb005eec788677c5e886160b772
Scott Young <syoung@instructure.com>
2020-10-14 14:15:58 -0600

    add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |_reg|
-  Registration.create(
-    user_id: User.last.id,
-    uid: User.last.uid,
-    live_course_id: rand(20),
-    live_course_session_id: rand(30),
-    bridge_registration_id: rand(500)
+20.times do
+  user = User.create(
+    uid: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
   )
+
+  10.times do
+    Registration.create(
+      live_course_id: rand(),
+      live_course_session_id: rand(),
+      bridge_registration_id: rand(),
+      user_id: user.id,
+      uid: user.uid,
+      is_attended: [true, false].sample,
+      live_course_title: Faker::DcComics.title,
+      session_start_time: Faker::Time.between_dates(from: Date.today - 20, to: Date.today, period: :all),
+    )
+  end
 end
